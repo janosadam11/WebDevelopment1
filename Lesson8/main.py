a13386210fa932a91961b3134caabc93228c4fcc
janosadam11 <janosadamnagy91@gmail.com>
2019-04-04 17:46:42 +0200

    Mood checker

---
+++ Lesson8/main.py
@@ -0,0 +1,15 @@
+mood = input("How do you feel? \n")
+
+if mood=="happy":
+    print("It is great to see you happy!")
+elif(mood=="nervous"):
+    print("Take a deep breath 3 times.")
+elif(mood=="sad"):
+    print("Im sorry about that :(")
+else:
+    print("Moodless")
+
+
+
+
+
