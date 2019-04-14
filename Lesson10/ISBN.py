54cd3e3c73f8e819d4f5a263e5e2b3b7c8a9b4a4
janosadam11 <janosadamnagy91@gmail.com>
2019-04-14 12:34:12 +0200

    Lesson10

--- Lesson10/ISBN.py
+++ Lesson10/ISBN.py
@@ -2,21 +2,21 @@ import operator
 
 digits = []
 numbers = [10, 9, 8, 7, 6, 5, 4, 3, 2]
-addlist = []
-checkdigit = 0
+mul_list = []
+check_digit = 0
 
 isbn = input("Give the first 9 digits for 10 digit ISBN number: ")
 digits = [int(i) for i in isbn]
-addlist = list(map(operator.mul, digits, numbers))
+mul_list = list(map(operator.mul, digits, numbers))
 
 result = 0
-for j in addlist:
+for j in mul_list:
     result += j
 
 if result % 11 == 0:
-    checkdigit = 0
+    check_digit = 0
 else:
-    checkdigit = (result + (11 - (result % 11))) - result
+    check_digit = (result + (11 - (result % 11))) - result
 
-print("The checkdigit is:", checkdigit)
-print("The full ISBN number is: ISBN", isbn,"-", checkdigit)
+print("The checkdigit is:", check_digit)
+print("The full ISBN number is: ISBN", isbn,"-", check_digit)
