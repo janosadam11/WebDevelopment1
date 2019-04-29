14ae007002c83b333976312cf37637cf02060bec
janosadam11 <janosadamnagy91@gmail.com>
2019-04-29 19:27:45 +0200

    Update

--- Lesson_13/main_oop.py
+++ Lesson_13/main_oop.py
@@ -1,6 +1,16 @@
 from Lesson_13.BasketballPlayer import BasketballPlayer
 
+
+
 kevin = BasketballPlayer("Kevin", "Lebron", 201, 118, 7.2, 7.4, 8.1)
 
-print(kevin.first_name)
+print("Create a Player with the following details:")
+kevin.first_name = input("First name of the player: ")
+kevin.last_name = input("Last name of the player: ")
+kevin.height_cm = input("Height: ")
+kevin.weight_kg = input("Weight: ")
+
+print("Your created Player:", kevin.first_name, kevin.last_name, kevin.height_cm, kevin.weight_kg)
+
+
 
