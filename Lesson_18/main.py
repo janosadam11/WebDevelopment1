2f944a6ee9f2b287d17933efa19a3ec3c41b939d
janosadam11 <janosadamnagy91@gmail.com>
2019-05-22 21:57:54 +0200

    Update

--- Lesson_18/main.py
+++ Lesson_18/main.py
@@ -1,42 +1,42 @@
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, redirect, url_for, make_response
+from smartninja_nosql.odm import Model
 
 app = Flask(__name__)
 
+class User(Model):
+    def __init__(self, name, email, **kwargs):
+        self.name = name
+        self.email = email
 
-@app.route("/", methods=["GET"])
+        super().__init__(**kwargs)
+
+@app.route("/")
 def index():
-    page_title = "Homepage"
+    email_address = request.cookies.get("email")
 
-    cities = ["Wien", "Prag", "Bratislava", "London"]
-
-    return render_template("index.html", page_title=page_title, cities=cities)
-
-
-@app.route("/impressum")
-def impressum():
-    return render_template("impressum.html")
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact", methods=["GET","POST"])
-def success():
-    if request.method == "GET":
-        user_name = request.cookies.get("user_name")
-        return render_template("success.html", name=user_name)
+    if email_address:
+        user = User.fetch_one(query=["email", "==", email_address])
     else:
-        contact_name = request.form.get("contact-name")
-        contact_mail = request.form.get("contact-email")
-        contact_text = request.form.get("contact-message")
+        user = None
 
-        response = make_response(render_template("success.html", name=contact_name, mail=contact_mail, user_text=contact_text))
-        response.set_cookie("user_name", contact_name)
-
-        return response
+    return render_template("index.html", user=user)
 
 
-if __name__ == "__main__":
-    app.debug = True
+@app.route("/login", methods=["POST"])
+def login():
+    name = request.form.get("user-name")
+    email = request.form.get("user-email")
+
+    # create a User object
+    user = User(name=name, email=email)
+    user.create()  # save the object into a database
+
+    # save user's email into a cookie
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie("email", email)
+
+    return response
+
+
+if __name__ == '__main__':
     app.run()
